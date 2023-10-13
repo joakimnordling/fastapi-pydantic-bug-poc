c02dfb839a4fca94175f2da27c14873024f76494
Joakim Nordling <joakim.nordling@ioxio.com>
2023-10-13 15:40:03 +0300

    Change importing

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from typing import List
+import importlib
+from pathlib import Path
 
 from fastapi import FastAPI
-from pydantic import BaseModel, ConfigDict, Field
-from stringcase import camelcase
 
 import routes
 
@@ -14,53 +13,16 @@ app = FastAPI(
 app.include_router(routes.router)
 
 
-class CamelCaseModel(BaseModel):
-    model_config = ConfigDict(alias_generator=camelcase, populate_by_name=True)
+p = Path("models.py")
+spec = importlib.util.spec_from_file_location(name=str(p), location=str(p))
 
+if not spec.loader:
+    raise RuntimeError(f"Failed to import {p} module")
 
-class CurrentAirQualityRequest(CamelCaseModel):
-    lat: float = Field(
-        ...,
-        title="Latitude",
-        description="The latitude coordinate of the desired location",
-        examples=[60.192059],
-        ge=-90,
-        le=90,
-    )
-    lon: float = Field(
-        ...,
-        title="Longitude",
-        description="The longitude coordinate of the desired location",
-        examples=[24.945831],
-        ge=-180,
-        le=180,
-    )
+module = spec.loader.load_module(str(p))
 
-
-class CurrentAirQualityResponse(CamelCaseModel):
-    air_quality_index: int = Field(
-        ...,
-        title="Air Quality Index",
-        description=(
-            "Current air quality index.\nRanges:\n0-50 Good;\n51-100 Moderate;\n"
-            "101-150 Unhealthy For Sensitive Groups;\n151-200 Unhealthy;\n"
-            "201-300 Very Unhealthy;\n301+ Hazardous"
-        ),
-        ge=0,
-        examples=[30],
-    )
-    timestamp: str = Field(
-        ...,
-        title="Timestamp",
-        description="Current timestamp in RFC 3339 format",
-        examples=["2020-04-03T13:00:00Z"],
-    )
-    attribution: List[str] = Field(
-        ...,
-        title="Source Attribution",
-        description="List of text to show required credits to data sources",
-        examples=[["XYZ environmental monitoring"]],
-    )
+CurrentAirQualityResponse = getattr(module, "CurrentAirQualityResponse")
+CurrentAirQualityRequest = getattr(module, "CurrentAirQualityRequest")
 
 
 @app.get("/")
