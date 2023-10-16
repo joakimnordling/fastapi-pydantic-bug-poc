43f4290c3597ff9e5e097da845c3ced139341200
Joakim Nordling <joakim.nordling@ioxio.com>
2023-10-16 15:32:24 +0300

    Smaller example

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 import importlib
+import json
 from pathlib import Path
-
-from fastapi import FastAPI
-
-import routes
-
-app = FastAPI(
-    title="Air Quality Index",
-    description="Data Product for current air quality index",
-    version="1.0.0",
-)
-app.include_router(routes.router)
-
 
 p = Path("models.py").absolute()
 spec = importlib.util.spec_from_file_location(name=str(p), location=str(p))
@@ -24,19 +13,4 @@ module = spec.loader.load_module(str(p))
 CurrentAirQualityResponse = getattr(module, "CurrentAirQualityResponse")
 CurrentAirQualityRequest = getattr(module, "CurrentAirQualityRequest")
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.post(
-    "/AirQuality/Current",
-    response_model=CurrentAirQualityResponse,
-)
-def read_item(data: CurrentAirQualityRequest):
-    return CurrentAirQualityResponse(
-        air_quality_index=30,
-        timestamp="2020-04-03T13:00:00Z",
-        attribution=["XYZ environmental monitoring"],
-    )
+print(json.dumps(CurrentAirQualityRequest.model_json_schema(), indent=2))
